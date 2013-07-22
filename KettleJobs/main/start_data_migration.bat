37d9d817bba708b13635c51b418b2954099ef712
Nayan Ambali <nayan.ambali@gmail.com>
2013-07-22 18:20:18 +0530

    minor changes

--- KettleJobs/main/start_data_migration.bat
+++ KettleJobs/main/start_data_migration.bat
@@ -11,5 +11,5 @@ mysql -uroot -pmysql %DESTDB% < %KETTLEPATH%main\load_mifosx_ddl.sql
 mysql -uroot -pmysql %DESTDB% < %KETTLEPATH%main\load_mifosx_datatables.sql
 mysql -uroot -pmysql %DESTDB% < %KETTLEPATH%main\load_mifosx_migration_stored_procedures.sql
 
-%PDIPATH%kitchen.bat /file:%KETTLEPATH%Stage1\Stage1.kjb /level:Basic > transformationLog_%DESTDB%.log
+%PDIPATH%kitchen.bat /file:%KETTLEPATH%Stage1\Stage1.kjb /level:Error > transformationLog_%DESTDB%.log
 
